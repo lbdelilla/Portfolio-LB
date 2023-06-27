d15ee2e6c42f9949f1221bef7d49f023cf5483e4
lbdelilla <lbdelilla@gmail.com>
2023-06-27 10:45:48 +0000

    fix: separated the google analytics script for correct building

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
+import { setupGoogleAnalytics } from "../utils/google-analytics"; 
 
 export default function Document() {
   return (
@@ -9,29 +10,34 @@ export default function Document() {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
           integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-          crossorigin="anonymous"
-          referrerpolicy="no-referrer"
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
         />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HXBCFCGYF2"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-
-          gtag('config', 'G-HXBCFCGYF2');
-        </script>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-HXBCFCGYF2" />
+        <script dangerouslySetInnerHTML={{ __html: `(${setupGoogleAnalytics.toString()})()` }} />
       </Head>
       <body>
-        <Script id="theme-switcher" strategy='beforeInteractive'>
-          {`
-            if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-            document.documentElement.classList.add('dark')
-            } else {
-            document.documentElement.classList.remove('dark')
-            }
-            `}
-        </Script>
-        <Script src="https://kit.fontawesome.com/beecaece23.js" crossorigin="anonymous"></Script>
+        <Script
+          id="theme-switcher"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              if (
+                localStorage.theme === 'dark' ||
+                (!('theme' in localStorage) &&
+                  window.matchMedia('(prefers-color-scheme: dark)').matches)
+              ) {
+                document.documentElement.classList.add('dark');
+              } else {
+                document.documentElement.classList.remove('dark');
+              }
+            `,
+          }}
+        />
+        <Script
+          src="https://kit.fontawesome.com/beecaece23.js"
+          crossOrigin="anonymous"
+        />
         <Main />
         <NextScript />
       </body>
