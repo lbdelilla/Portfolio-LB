f2b2b147bdf29d0018f5ed2fc5aeb8ca7ba31cb2
lbdelilla <lbdelilla@gmail.com>
2023-06-27 10:50:07 +0000

    fix: new tag id

--- src/utils/google-analytics.js
+++ src/utils/google-analytics.js
@@ -2,5 +2,5 @@ export function setupGoogleAnalytics() {
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', 'G-HXBCFCGYF2');
+    gtag('config', 'G-T9MS8F7Y5G');
   }
